ce75031f7364839578e875bfac1d0a97366db681
Harish Soni <hksoni.9312@gmail.com>
2020-08-20 15:07:20 +0530

    Fixed homepage styling

---
+++ precache-manifest.f07e2d02324b3490144db5438d41da91.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8eec0d6200ec08bad5be46f47063b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb789e3c8e9aaa09892c",
+    "url": "/static/css/1.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "979415d9c333fd3f172c",
+    "url": "/static/css/4.1809473b.chunk.css"
+  },
+  {
+    "revision": "cb9a8341c5f698f4fc71",
+    "url": "/static/css/5.7b8bee9c.chunk.css"
+  },
+  {
+    "revision": "87a988b2bd9f473a915b",
+    "url": "/static/css/6.822bd1bc.chunk.css"
+  },
+  {
+    "revision": "b28790f063bfb0dae278",
+    "url": "/static/css/7.7b8bee9c.chunk.css"
+  },
+  {
+    "revision": "1477247d26d96ab2b0be",
+    "url": "/static/css/main.59373edf.chunk.css"
+  },
+  {
+    "revision": "40e1292579e35fa82cf2",
+    "url": "/static/js/0.85830a9e.chunk.js"
+  },
+  {
+    "revision": "fb789e3c8e9aaa09892c",
+    "url": "/static/js/1.db0a6423.chunk.js"
+  },
+  {
+    "revision": "979415d9c333fd3f172c",
+    "url": "/static/js/4.acbac433.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/static/js/4.acbac433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9a8341c5f698f4fc71",
+    "url": "/static/js/5.72981f4e.chunk.js"
+  },
+  {
+    "revision": "87a988b2bd9f473a915b",
+    "url": "/static/js/6.beb370c1.chunk.js"
+  },
+  {
+    "revision": "b28790f063bfb0dae278",
+    "url": "/static/js/7.20998e91.chunk.js"
+  },
+  {
+    "revision": "1477247d26d96ab2b0be",
+    "url": "/static/js/main.e623e4e2.chunk.js"
+  },
+  {
+    "revision": "603cef2830f29e0567b0",
+    "url": "/static/js/runtime-main.1f194228.js"
+  },
+  {
+    "revision": "80eb678f48affa3d9556c646ca1912d2",
+    "url": "/static/media/1.80eb678f.jpg"
+  },
+  {
+    "revision": "5fa59804e02eafb6de7f186bb606e694",
+    "url": "/static/media/2.5fa59804.jpg"
+  },
+  {
+    "revision": "48d29d21ca09ed0132b66e8f0778ac05",
+    "url": "/static/media/3.48d29d21.jpg"
+  },
+  {
+    "revision": "a94e7f002f08686cb0981a3229b86186",
+    "url": "/static/media/f1.a94e7f00.jpg"
+  },
+  {
+    "revision": "ac434b7f1eb222faabee6f804d97a538",
+    "url": "/static/media/f2.ac434b7f.jpg"
+  },
+  {
+    "revision": "612eb0a6451e96c4ed681a52e73b0638",
+    "url": "/static/media/f3.612eb0a6.jpg"
+  },
+  {
+    "revision": "cfcdb9778db198f1cdacba8b917a3713",
+    "url": "/static/media/fabLogo.cfcdb977.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6097caddb8d0801eedea5f49198da7a7",
+    "url": "/static/media/t1.6097cadd.jpg"
+  },
+  {
+    "revision": "1c775f4414c531d7d7e2ee08adf00de8",
+    "url": "/static/media/t2.1c775f44.jpg"
+  },
+  {
+    "revision": "30e2b124ce90dd435f733ccaa47e86c1",
+    "url": "/static/media/t3.30e2b124.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cacca2b219d943efe6b5896f46a5992.js"
+  "/precache-manifest.f07e2d02324b3490144db5438d41da91.js"
 );
 
 self.addEventListener('message', (event) => {
