4fb067c0faa735571ea24e0ac489fb517fc7fa8d
Harish Soni <hksoni.9312@gmail.com>
2020-08-22 22:57:45 +0530

    Added New Build

---
+++ precache-manifest.669f08ce608e86b3effa30ef3afb9b98.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e52e8574666ab747c3fe584e4df5a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f22613852476cb1dd0",
+    "url": "/static/css/0.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "2a99d240e1545575363b",
+    "url": "/static/css/3.1809473b.chunk.css"
+  },
+  {
+    "revision": "820d09171de9a048d11a",
+    "url": "/static/css/4.966139b9.chunk.css"
+  },
+  {
+    "revision": "a2b6005dca45449eb170",
+    "url": "/static/css/5.01e4b33e.chunk.css"
+  },
+  {
+    "revision": "dd3194ee96a3d1e5cde6",
+    "url": "/static/css/6.39b735cd.chunk.css"
+  },
+  {
+    "revision": "d540633d5cd2e15aae6c",
+    "url": "/static/css/main.36d23b73.chunk.css"
+  },
+  {
+    "revision": "15f22613852476cb1dd0",
+    "url": "/static/js/0.64311c6a.chunk.js"
+  },
+  {
+    "revision": "2a99d240e1545575363b",
+    "url": "/static/js/3.c290847f.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/static/js/3.c290847f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820d09171de9a048d11a",
+    "url": "/static/js/4.75fe4fc3.chunk.js"
+  },
+  {
+    "revision": "a2b6005dca45449eb170",
+    "url": "/static/js/5.6ed490f7.chunk.js"
+  },
+  {
+    "revision": "dd3194ee96a3d1e5cde6",
+    "url": "/static/js/6.d77e5395.chunk.js"
+  },
+  {
+    "revision": "d540633d5cd2e15aae6c",
+    "url": "/static/js/main.1d262fed.chunk.js"
+  },
+  {
+    "revision": "23a3227ffebfbc51c621",
+    "url": "/static/js/runtime-main.6fe00d41.js"
+  },
+  {
+    "revision": "80eb678f48affa3d9556c646ca1912d2",
+    "url": "/static/media/1.80eb678f.jpg"
+  },
+  {
+    "revision": "5fa59804e02eafb6de7f186bb606e694",
+    "url": "/static/media/2.5fa59804.jpg"
+  },
+  {
+    "revision": "48d29d21ca09ed0132b66e8f0778ac05",
+    "url": "/static/media/3.48d29d21.jpg"
+  },
+  {
+    "revision": "a94e7f002f08686cb0981a3229b86186",
+    "url": "/static/media/f1.a94e7f00.jpg"
+  },
+  {
+    "revision": "ac434b7f1eb222faabee6f804d97a538",
+    "url": "/static/media/f2.ac434b7f.jpg"
+  },
+  {
+    "revision": "612eb0a6451e96c4ed681a52e73b0638",
+    "url": "/static/media/f3.612eb0a6.jpg"
+  },
+  {
+    "revision": "cfcdb9778db198f1cdacba8b917a3713",
+    "url": "/static/media/fabLogo.cfcdb977.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7868f93371b657b49036daa6323477d8",
+    "url": "/static/media/p1.7868f933.jpg"
+  },
+  {
+    "revision": "2ec5a88ba9245bc8f9bd747a6b413ced",
+    "url": "/static/media/p2.2ec5a88b.jpg"
+  },
+  {
+    "revision": "0eabea65982470bfa3008e9ce427c954",
+    "url": "/static/media/p3.0eabea65.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6097caddb8d0801eedea5f49198da7a7",
+    "url": "/static/media/t1.6097cadd.jpg"
+  },
+  {
+    "revision": "1c775f4414c531d7d7e2ee08adf00de8",
+    "url": "/static/media/t2.1c775f44.jpg"
+  },
+  {
+    "revision": "30e2b124ce90dd435f733ccaa47e86c1",
+    "url": "/static/media/t3.30e2b124.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfff4451b1a2882e5c5195f326a530d3.js"
+  "/precache-manifest.669f08ce608e86b3effa30ef3afb9b98.js"
 );
 
 self.addEventListener('message', (event) => {
