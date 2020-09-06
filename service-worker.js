9173eb6916dd6e0a4a344acfa9c1087ae9b82588
Harish Soni <hksoni.9312@gmail.com>
2020-09-06 17:30:01 +0530

    Added New Build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88647e48564ff4cf5136550335485ceb.js"
+  "/precache-manifest.33d7c1207ca61e32125ccce82add61d6.js"
 );
 
 self.addEventListener('message', (event) => {
